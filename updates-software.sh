c87c99364aacccac1018990583449e71ca7a811f
hsuacap <39367803+hsuacap@users.noreply.github.com>
2019-03-11 17:14:35 +0800

    Update updates-software.sh

--- updates-software.sh
+++ updates-software.sh
@@ -3,7 +3,7 @@ set x
 systemctl stop firewalld
 systemctl mask firewalld
 yum groupinstall "Development Tools" -y
-yum install psmisc net-tools systemd-devel libdb-devel perl-DBI xfsprogs rsyslog logrotate crontabs file vim locate -y
+yum install psmisc net-tools systemd-devel libdb-devel perl-DBI xfsprogs rsyslog logrotate crontabs file vim locate wget -y
 yum update -y
 
 echo 'Done installing softwares and updates, plese reboot the server !"
